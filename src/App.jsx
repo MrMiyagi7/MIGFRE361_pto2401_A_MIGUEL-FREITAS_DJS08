6f736791b81a27ccf2525d5145b7ac44937ab304
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-13 22:26:03 +0200

    Completed Scrimba code along

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./About.jsx";
+import Home from "./Home.jsx";
+import Vans from "./vans.jsx";
+import VanDetail from "./VansDetail";
+import "./server.js";
+import Layout from "../components/Layout.jsx";
+import Dashboard from "./Host/Dashboard.jsx";
+import Reviews from "./Host/Reviews.jsx";
+import Income from "./Host/Income.jsx";
+import HostLayout from "../components/HostLayout.jsx";
+import HostVans from "./Host/HostVans.jsx";
+import HostVanDetail from "./Host/HostVanDetail";
+import HostVanInfo from "./Host/HostVanInfo";
+import HostVanPricing from "./Host/HostVanPricing";
+import HostVanPhotos from "./Host/HostVanPhotos";
 function App() {
-
   return (
-    <h1>Start here</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
